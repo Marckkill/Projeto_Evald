d9f6b962fdb9536512dcb4bc2eea98f06437049c
José Fonseca <marckkill.plays@gmail.com>
2023-08-22 16:18:33 -0300

    set q and r

--- HHO.m
+++ HHO.m
@@ -31,7 +31,7 @@ while t<T
             %% Exploration:
             % Harris' hawks perch randomly based on 2 strategy:
             
-            q=rand(); 
+            q=0.4; 
             rand_Hawk_index = floor(N*rand()+1);
             X_rand = X(rand_Hawk_index, :);
             if q<0.5
@@ -49,7 +49,7 @@ while t<T
              %% phase 1: surprise pounce (seven kills)
             % surprise pounce (seven kills): multiple, short rapid dives by different hawks
             
-            r=rand(); % probablity of each event
+            r=0.4; % probablity of each event
             
             if r>=0.5 && abs(Escaping_Energy)<0.5 % Hard besiege
                 X(i,:)=(Rabbit_Location)-Escaping_Energy*abs(Rabbit_Location-X(i,:));
