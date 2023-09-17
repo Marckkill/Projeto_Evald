904e2dae4d536254e61ac6aa5563b3b758779d2c
José Fonseca <marckkill.plays@gmail.com>
2023-09-17 16:08:00 -0300

    Troca do Kappa

--- Get_Functions_details.m
+++ Get_Functions_details.m
@@ -2,7 +2,7 @@ function [lb,ub,dim,fobj] = Get_Functions_details(F)
 switch F       
     case 'F1'
         fobj = @F1;
-        lb = [  1,    1,   -20,   -20,   -20,   -20,  -20,  -20,  -20 ];
+        lb = [  1,    500,   -20,   -20,   -20,   -20,  -20,  -20,  -20 ];
         ub = [ 1e3,  1e3,   20,    20,    20,    20,   20,   20,   20 ]; 
         dim=9;  
 end
--- main.m
+++ main.m
@@ -2,14 +2,55 @@ clear all %#ok<CLALL>
 close all
 clc
 
-N=20; %Numero de falcoes
-Function_name='F1'; %Sistema  
-T=200; %Numero de iteracoes
 
-num = 10; %Numero de vezes que o codigo roda
-logFilename = 'console_output_log.txt';
+Function_name='F1'; %Sistema  
+
+
+num = 5; %Numero de vezes que o codigo roda
+logFilename = 'new_console_output_log.txt';
 diary(logFilename); 
 
+N=10; 
+T=100; 
+for run = 1:num
+    [lb,ub,dim,fobj]=Get_Functions_details(Function_name);
+    [Rabbit_Energy,Rabbit_Location,CNVG]=HHO(N,T,lb,ub,dim,fobj);
+    display(['Loop: ', num2str(run)]);
+    display(['Numero de falcoes: ', num2str(N)]);
+    display(['Numero de iteracoes: ', num2str(T)]);
+    display(['Valores otimizados: ', num2str(Rabbit_Location)]);
+    display(['Melhor fitness: ', num2str(Rabbit_Energy)]);
+    display(' ');
+end
+
+N=5;
+T=100;
+for run = 1:num
+    [lb,ub,dim,fobj]=Get_Functions_details(Function_name);
+    [Rabbit_Energy,Rabbit_Location,CNVG]=HHO(N,T,lb,ub,dim,fobj);
+    display(['Loop: ', num2str(run)]);
+    display(['Numero de falcoes: ', num2str(N)]);
+    display(['Numero de iteracoes: ', num2str(T)]);
+    display(['Valores otimizados: ', num2str(Rabbit_Location)]);
+    display(['Melhor fitness: ', num2str(Rabbit_Energy)]);
+    display(' ');
+end
+
+N=10; 
+T=200; 
+for run = 1:num
+    [lb,ub,dim,fobj]=Get_Functions_details(Function_name);
+    [Rabbit_Energy,Rabbit_Location,CNVG]=HHO(N,T,lb,ub,dim,fobj);
+    display(['Loop: ', num2str(run)]);
+    display(['Numero de falcoes: ', num2str(N)]);
+    display(['Numero de iteracoes: ', num2str(T)]);
+    display(['Valores otimizados: ', num2str(Rabbit_Location)]);
+    display(['Melhor fitness: ', num2str(Rabbit_Energy)]);
+    display(' ');
+end
+
+N=5;
+T=200;
 for run = 1:num
     [lb,ub,dim,fobj]=Get_Functions_details(Function_name);
     [Rabbit_Energy,Rabbit_Location,CNVG]=HHO(N,T,lb,ub,dim,fobj);
